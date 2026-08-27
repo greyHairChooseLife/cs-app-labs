2eb45da951340f7ab49d28bd71ae120294f5bf68
sangyeon kim <greyhairchooselife@gmail.com>
2026-08-28 00:30:51 +0900

    format:

--- handout/datalab-handout/bits.c
+++ handout/datalab-handout/bits.c
@@ -222,8 +222,9 @@ int negate(int x)
 }
 // 3
 /*
- * isAsciiDigit - return 1 if 0x30 <= x <= 0x39 (ASCII codes for characters '0'
- * to '9') Example: isAsciiDigit(0x35) = 1. isAsciiDigit(0x3a) = 0.
+ * isAsciiDigit - return 1 if 0x30 <= x <= 0x39 (ASCII codes for characters '0' to '9')
+ *   Example: isAsciiDigit(0x35) = 1.
+ *            isAsciiDigit(0x3a) = 0.
  *            isAsciiDigit(0x05) = 0.
  *   Legal ops: ! ~ & ^ | + << >>
  *   Max ops: 15
